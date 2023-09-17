8530febd4376923277d99da93ffd7e01beaa0c78
Josue Sebastiao <josuesalazaku@gmail.com>
2023-09-17 12:31:35 +0200

    to-do-app2

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 //de variables inputBox en listContainers zijn element/ id's die ik dus van mijn  htlm in javascript binnen breng om functioneel te maken //
-const inputBox = document.getElementById('iput-box');
+const inputBox = document.getElementById('input-box');
 const listContainer = document.getElementById('list-container');
 
 function addTask() {
-    if(inputBox.value === '') {
+    if (inputBox.value === '') {
         alert("you must write something bro!");
+    } else {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
     }
-        else {
-            let li = document.createElement("li");
-            li.innerHTML = inputBox.value;
-            listContainer.appendChild(li);
-        }
 }
